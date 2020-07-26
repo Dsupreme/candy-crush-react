671b73ae831b48f9173823ad7b54a828a801da70
Manmeet Oberoi <manmeet.s.oberoi@gmail.com>
2020-07-26 21:22:22 +0530

    Updates

---
+++ precache-manifest.9f675569be8217302f57cd01deaa2556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa19fe7f6e5a60103bb8535cea9ecfb",
+    "url": "/candy-crush-react/index.html"
+  },
+  {
+    "revision": "f66cef91874bb0b735c1",
+    "url": "/candy-crush-react/static/css/main.f6a3cde4.chunk.css"
+  },
+  {
+    "revision": "00536439cba2199f3ddc",
+    "url": "/candy-crush-react/static/js/2.43f790a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/candy-crush-react/static/js/2.43f790a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66cef91874bb0b735c1",
+    "url": "/candy-crush-react/static/js/main.3a32f916.chunk.js"
+  },
+  {
+    "revision": "5e1f6dc2f6ffdf6a8e4a",
+    "url": "/candy-crush-react/static/js/runtime-main.f31b2aa7.js"
+  }
+]);
